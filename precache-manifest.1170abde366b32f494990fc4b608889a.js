1a235001de590e4ce5df754c0e2f1453eb64e398
daotrunghieu86 <daotrunghieu08061997@gmail.com>
2020-08-04 16:24:31 +0700

    Updates

---
+++ precache-manifest.1170abde366b32f494990fc4b608889a.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "517b8c18835916f3eddb70d6ab865b28",
+    "url": "/Blog-React.git/index.html"
+  },
+  {
+    "revision": "495bb622a23618f1f7e7",
+    "url": "/Blog-React.git/static/css/main.732e0d7e.chunk.css"
+  },
+  {
+    "revision": "b2b15e35ff0c70d1b36c",
+    "url": "/Blog-React.git/static/js/2.41ae1098.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/Blog-React.git/static/js/2.41ae1098.chunk.js.LICENSE"
+  },
+  {
+    "revision": "495bb622a23618f1f7e7",
+    "url": "/Blog-React.git/static/js/main.a3cab273.chunk.js"
+  },
+  {
+    "revision": "a5880b5c34f23b41c65a",
+    "url": "/Blog-React.git/static/js/runtime-main.4244c162.js"
+  },
+  {
+    "revision": "80961a00ad954f1df9a3356d8e386900",
+    "url": "/Blog-React.git/static/media/affection-baby-baby-girl-beautiful-377058.80961a00.jpg"
+  },
+  {
+    "revision": "4218ad9257e6d8b09e88ad06c8308e04",
+    "url": "/Blog-React.git/static/media/beautiful-&-simple.4218ad92.jpg"
+  },
+  {
+    "revision": "a67df1389674b05ae6d1b50ad59a30b1",
+    "url": "/Blog-React.git/static/media/cestovat-chladny-dno-jednoduchost-2868847.a67df138.jpg"
+  },
+  {
+    "revision": "45c60c8a7cce7c53f5988fcdb2b71656",
+    "url": "/Blog-React.git/static/media/fitness-blog-post.45c60c8a.jpg"
+  },
+  {
+    "revision": "13ad316bf606c371e9f32ad600e9f949",
+    "url": "/Blog-React.git/static/media/memories-from.13ad316b.jpg"
+  }
+]);
